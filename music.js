f350aac59cb227900ac5156382b99286d71dceeb
Haley B-E <hbrandterichsen@gmail.com>
2024-04-10 17:18:53 -0400

    implemented Show

--- music.js
+++ music.js
@@ -34,6 +34,18 @@ const markAsPlayed = (title) => {
         currentSong.hasBeenPlayed = true;
     }
 }
+
+const getLibrary = (limitToUnplayed = false, artistFilter = null) => {
+    return library.filter(song => {
+        if (limitToUnplayed && song.hasBeenPlayed) {
+            return false;
+        }
+        if (artistFilter && song.artist !== artistFilter) {
+            return false;
+        }
+        return true;
+    })
+}
 //#endregion
 
 //#region main REPL
@@ -53,7 +65,11 @@ rl.on("line", answer => {
             break;
         }
         case "show": { // show all; show unplayed; show all by "$artist"; show unplayed by "$artist"
-            //TO DO
+            let artistFilter;
+            if (commandAndArgs[2] && commandAndArgs[2] === "by") {
+                artistFilter = commandAndArgs[3];
+            }
+            show(commandAndArgs[1], artistFilter)
             break;
         }
         case "quit": {
@@ -94,6 +110,35 @@ const play = (title) => {
         }
     } else {
         console.log("Invalid input - required format is 'play \"$title\"");
+    }
+    rl.prompt();
+}
+
+const show = (whatToShow, artistFilter) => {
+    const isUnplayedFilterValid = (whatToShow === "all" || whatToShow === "unplayed");
+    if (!isUnplayedFilterValid || (artistFilter && !verifyFormatting(artistFilter))) {
+        console.log("Invalid input - allowed formats: 'show all', 'show unplayed', 'show all by \"$artist\", 'show unplayed by \"$artist\"");
+    }
+    
+    if (artistFilter) {
+        artistFilter = stripQuotationMarks(artistFilter);
+    }
+    let songsToShow;
+    switch (whatToShow) {
+        case "all":
+            songsToShow = getLibrary(false, artistFilter);
+            songsToShow.forEach(song => {
+                console.log(`"${song.title}" by ${song.artist} (${song.hasBeenPlayed ? "played" : "unplayed"})`);
+            })
+            break;
+        case "unplayed":
+            songsToShow = getLibrary(true, artistFilter);
+            songsToShow.forEach(song => {
+                console.log(`"${song.title}" by ${song.artist}`);
+            })
+            break;
+        default:
+            console.log(invalidFormatResponse);
     }
     rl.prompt();
 }
