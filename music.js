41fca053dec5c996b9aa8f519820a7a2c70e12be
Haley B-E <hbrandterichsen@gmail.com>
2024-04-10 16:53:15 -0400

    handle Play command

--- music.js
+++ music.js
@@ -4,8 +4,7 @@ const { stdin: input, stdout: output } = require('process');
 
 const rl = readline.createInterface({ input, output });
 
-// format: {title: string, artist: string, hasBeenPlayed: bool}
-const library = [];
+//#region helpers - input parsing
 
 // per provided specs, title and artist should be surrounded by quotation marks
 // if I were making this production-ready, I'd probably make the quotation marks optional and maybe
@@ -19,40 +18,84 @@ const stripQuotationMarks = (word) => {
     return word.slice(1,-1);
 }
 
-// begin main REPL
+//#endregion
+
+//#region library management 
+// format: {title: string, artist: string, hasBeenPlayed: bool}
+const library = [];
+
+const verifyTitleInLibrary = (title) => {
+    return library.some(song => song.title === title);
+}
+
+const markAsPlayed = (title) => {
+    if (verifyTitleInLibrary(title)) {
+        const currentSong = library.filter(song => song.title === title)[0];
+        currentSong.hasBeenPlayed = true;
+    }
+}
+//#endregion
+
+//#region main REPL
 console.log("Welcome to your music collection! \n");
 rl.setPrompt("> ");
 rl.prompt();
 rl.on("line", answer => {
     const commandAndArgs = answer.split(" "); //first word is command; subsequent words are arguments
+    //TODO: HANDLE TOO MANY INPUTS
     switch (commandAndArgs[0]) {
-        case "add": //add "$title" "$artist"
-            let title = commandAndArgs[1];
-            let artist = commandAndArgs[2];
-            if (title && artist && verifyFormatting(title) && verifyFormatting(artist)) {
-                title = stripQuotationMarks(title);
-                artist = stripQuotationMarks(artist);
-                library.push({
-                    title: title, 
-                    artist: artist, 
-                    hasBeenPlayed: false});
-                console.log(`Added "${title}" by ${artist}`)
-                rl.prompt();
-            } else {
-                console.log("Invalid input - required format is 'add \"$title\" \"$artist\"");
-            }
-            //TO DO
-        case "play": // play "$title"
-            //TO DO - PLAY $TITLE
+        case "add": { //add "$title" "$artist"
+            add(commandAndArgs[1], commandAndArgs[2]);
             break;
-        case "show": // show all; show unplayed; show all by "$artist"; show unplayed by "$artist"
+        }
+        case "play": { // play "$title"
+            play(commandAndArgs[1]);
+            break;
+        }
+        case "show": { // show all; show unplayed; show all by "$artist"; show unplayed by "$artist"
             //TO DO
             break;
-        case "quit":
+        }
+        case "quit": {
             console.log("Bye!");
             rl.close();
             break;
-        default:
+        }
+        default: {
             console.log("I didn't recognize that input.");
+            rl.prompt();
+        }
     }
 });
+
+const add = (title, artist) => {
+    if (title && artist && verifyFormatting(title) && verifyFormatting(artist)) {
+        title = stripQuotationMarks(title);
+        artist = stripQuotationMarks(artist);
+        library.push({
+            title: title, 
+            artist: artist, 
+            hasBeenPlayed: false});
+        console.log(`Added "${title}" by ${artist}`)
+    } else {
+        console.log("Invalid input - required format is 'add \"$title\" \"$artist\"");
+    }
+    rl.prompt();
+}
+
+const play = (title) => {
+    if (title && verifyFormatting(title)) {
+        title = stripQuotationMarks(title);
+        if (verifyTitleInLibrary(title)) {
+            console.log(`You're listening to "${title}"`);
+            markAsPlayed(title);
+        } else {
+            console.log(`Could not find "${title}" in library`)
+        }
+    } else {
+        console.log("Invalid input - required format is 'play \"$title\"");
+    }
+    rl.prompt();
+}
+
+//#endregion
